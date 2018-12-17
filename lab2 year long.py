9deedd147f377265f1580fa24ea0cebf82705fc6
yoavg20-meet <yoavg20@meet.mit.edu>
2018-12-17 12:29:16 -0500

    adding lab 2

---
+++ lab2 year long.py
@@ -0,0 +1,38 @@
+##a=[1,2,3,4,5,6,7,8,9]
+##b=[a[0],a[-1]]
+##
+##
+###2
+##
+##a=[1,1,2,3,5,8,13,21,34,55,89]
+##b=[]
+##for num in a:
+##    if (num<5):
+##        print num
+##        b.append(num)
+##        print b
+##
+###3
+##
+##        
+####
+####a= [1,1,2,3,5,8,13,21,34,55,89]
+####b= [1,2,3,4,5,6,7,8,9,10,12,13]
+####c= []
+####for i in a:
+####    if i in b and i not in c:
+####        c.append(i)
+####        ptint (c)
+##
+##
+##
+###4
+##
+##
+##
+##def ok (a):
+##    for i in range(2, a/2):
+##        if a%i==0:
+##            return False
+##        return True
+##print(ok(input()))
