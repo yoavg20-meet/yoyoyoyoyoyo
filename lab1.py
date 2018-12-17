db7a4ee5372348a98ca78318060ca65444c808fc
yoavg20-meet <yoavg20@meet.mit.edu>
2018-12-17 11:51:24 -0500

    Added lab1.py

---
+++ lab1.py
@@ -0,0 +1,29 @@
+#print ("yoav"*100)
+
+
+#num1 = 6
+
+
+#num2 = num1/2
+
+
+#print(num2)
+
+
+#yay=[4,5,6]
+#for i in yay:
+#    print (i*2)
+
+
+import turtle
+turtle.begin_fill()
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.end_fill()
+
+turtle.maimloop()
